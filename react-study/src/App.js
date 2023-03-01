d5ff2c5b265876bac114a435db29881b9844ad99
이시흔 <dudwo5600@naver.com>
2023-03-01 11:46:14 +0000

    react study (effects)

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,12 +1,32 @@
 
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState,useEffect} from "react";
 
 function App() {
+	const [value,setValue] = useState(0);
+	const [key,setKey]=useState("");
+	const onClick=()=>setValue((prev)=>prev+1);
+	const onChange=(event)=>setKey(event.target.value);
+
+	useEffect(()=>{
+		console.log("I run only once");
+	},[]);
+	useEffect(()=>{
+		console.log("I run when 'key' changes");
+	},[key]);
+	useEffect(()=>{
+		console.log("I run when 'value' changes");
+	},[value]);
   return (
     <div>
-		<h1 id={styles.title}>Welcome back!</h1>
-		<Button text={"Click"}></Button>
+		 <input 
+			 value={key}
+			 type="text" 
+			 placeholder="Search"
+			 onChange={onChange}></input>
+		<h1 id={styles.title}>{value}</h1>
+		<Button text={"Click"} onClick={onClick}></Button>
 	</div>
   );
 }
