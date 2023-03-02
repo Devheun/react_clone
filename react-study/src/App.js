47daf9df11817d0b86e9337b318f5fafcfcbaaf1
이시흔 <dudwo5600@naver.com>
2023-03-02 07:00:14 +0000

    react study (movie app 1)

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,42 +1,42 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 function App(){
 	
-	const [todo,setTodo] = useState("");
-	const [todos,setTodos] = useState([]);
-	
-	const onChange=(event)=>setTodo(event.target.value);
-	const onSubmit=(event)=>{
-		event.preventDefault()
-		if(todo===""){
-			return;
-		}
-		setTodo("");
-		setTodos((currentArray)=>[todo,...currentArray]);
+	const [loading,setLoading] = useState(true);
+	const [movies,setMovies] = useState([]);
+	const getMovies = async() =>{
+		const json = await (
+			await fetch(
+				`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+			)
+		).json();
+		setMovies(json.data.movies);
+		setLoading(false);
 	};
-	const deleteTodo=(index)=>{
-		setTodos((currentArray)=>currentArray.filter((_,curIndex)=>curIndex!==index));
-	}
-
+	
+	useEffect(()=>{
+		getMovies();
+	},[]);
+	
 	return(
 		<div>
-			<h1>To Do List ({todos.length})</h1>
-			<form onSubmit={onSubmit}>
-				<input 
-					type="text"
-					placeholder="Write your todo!"
-					onChange={onChange}
-					value={todo}>
-				</input>
-				<button>Add To Do</button>
-			</form>
-			<hr />
-			<ul>
-				{todos.map((item,index)=><li key={index}>{item}
-				<button onClick={()=>deleteTodo(index)}>❌</button></li>)}
-			</ul>
+			{loading ? <h1>Loading...</h1>:(
+			<div>
+				 {movies.map((movie)=>(
+					 <div key={movie.id}>
+						 <img src={movie.medium_cover_image}/>
+						 <h2>{movie.title}</h2>
+						 <p>{movie.summary}</p>
+						 <ul>
+							 {movie.genres && movie.genres.map((g)=>(
+							 	<li key={g}>{g}</li>
+							 ))}
+						 </ul>
+					 </div>
+				 ))}
+			</div>)}
 		</div>
-	)
+	);
 }
 
 export default App;
